003d2ab96e138f9c3b9867c62447b37380d79bac
Juan Manjarres <juanmanjarres@jumanjas-MacBook-Pro.local>
2018-10-25 07:36:03 -0500

    Enviando CAmbio en lcase User.js via nueva branch jmanjarres para probar el flujo de trabajo y posibles pull request en github

--- ui/js/class/user.js
+++ ui/js/class/user.js
@@ -1,4 +1,18 @@
+/*
+Clase: User
+
+Descripción: Servir de especificación para la construcción
+             de objetos de tipo Usuario de la Tabla users.
+*/
 class User {
+
+  /*
+  Método: constructor
+  Descripción: Recibe un objeto (usualmente el objeto es
+               la respuesta que devuelve el llamado a un
+               método de api) y pobla el objeto a crear
+               con la clase User.
+  */
   constructor(obj) {
     // Poblar la clase
     this._frat = obj.frat;
@@ -9,12 +23,20 @@ class User {
     this._email = obj.email;
     this._servicio = obj.servicio;
     this._tiposerv = obj.tiposerv;
+
+    //Si no se ha suministrado estado, será "A" (Activo)
     if(obj.estado == undefined){
       obj.estado = "A";
     }
     this._estado = obj.estado;
 
   }
+  /*
+  Métodos: getters y setters (get [prop], set [prop])
+  Descripción: Los métodos get recuperan el valor de la propiedad,
+               Los métodos set fijan o actualizan el valor de la propiedad.
+               
+  */
 
   get frat() {
     return this._frat;
@@ -88,8 +110,4 @@ class User {
     this._estado = estado;
   }
 
-  /*method1(...) {}
-  method2(...) {}
-  static staticMethod(..) {}
-  */
 }
